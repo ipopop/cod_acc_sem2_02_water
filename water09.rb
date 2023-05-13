c6d93ebf1d91bc4d99d1b2dc3b7935d9c0ccdc06
ipopop <popop@ntymail.com>
2023-05-13 23:57:44 +0200

    add water09.rb

---
+++ water09.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+## Between min and max
+
+# Create a program that displays all values between two numbers in ascending order. Min included, max excluded.
+
+# Display error and exit the program in case of argument problems.
+
+# Examples of use:
+
+# ```
+# $> ruby exo.py 20 25
+# 20 21 22 23 24
+# ```
+# ```
+# $> ruby exo.py 25 20
+# 20 21 22 23 24
+# ```
+# ```
+# $> ruby exo.py hello
+# error
+# ```
+
+
+def display_values(min, max)
+  (min...max).each { |n| print "#{n} " }
+  puts
+end
+
+def manual_sort(a, b)
+  if a < b
+    [a, b]
+  else
+    [b, a]
+  end
+end
+
+arg = ARGV
+
+if arg.length != 2 || !arg.all? { |arg| arg.match?(/^\d+$/) }
+  puts "error"
+  exit 1
+end
+
+min, max = manual_sort(arg[0].to_i, arg[1].to_i)
+display_values(min, max)
