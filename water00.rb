0baa8029dbff5cfaad83ba7f8bed25b59ef03ced
ipopop <popop@ntymail.com>
2023-05-17 14:49:52 +0200

    update tests/water00_test.rb

--- water00.rb
+++ water00.rb
@@ -3,32 +3,37 @@
 # Displays all the different possible combinations
 # of three numbers in ascending order, in ascending order (the repetition is voluntary).
 # Language: Ruby
-# Exec Command in Shell: ruby eau00.rb
+# Exec Command in Shell: ruby water00.rb
 
 
-# Define an empty array to store the valid combinations
-combinations = []
-
-# Loop through all numbers from 0 to 9 for the first digit
-(0..9).each do |i|
-  # Loop through all numbers from i to 9 for the second digit
-  (i..9).each do |j|
-    # Loop through all numbers from j to 9 for the third digit
-    (j..9).each do |k|
-      # Check if the combination is valid: all digits must be different
-      if i != j && i != k && j != k
-        # Check if the combination is not already in the array
-        unless combinations.include?([i, j, k])
-          # Add the combination to the array
-          combinations << [i, j, k]
+def generate_combinations
+  # Define an empty array to store the valid combinations
+  combinations = []
+  
+  # Loop through all numbers from 0 to 9 for the first digit
+  (0..9).each do |i|
+    # Loop through all numbers from i to 9 for the second digit
+    (i..9).each do |j|
+      # Loop through all numbers from j to 9 for the third digit
+      (j..9).each do |k|
+        # Check if the combination is valid: all digits must be different
+        if i != j && i != k && j != k
+          # Check if the combination is not already in the array
+          unless combinations.include?([i, j, k])
+            # Add the combination to the array
+            combinations << [i, j, k]
+          end
         end
       end
     end
   end
+  
+  # Return the combinations
+  combinations
 end
 
-# Format the combinations as a string
-result = combinations.map { |c| c.join('') }.join(', ')
+# Call the generate_combinations method and format the result
+result = generate_combinations.map { |c| c.join('') }.join(', ')
 
 # Print the result
 puts result
