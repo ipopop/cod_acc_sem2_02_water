816e10fb8ea07637aa2f06aa436662bb8bfacd9c
ipopop <popop@ntymail.com>
2023-05-21 21:01:16 +0200

    update water05_test.rb

--- tests/water05_test.rb
+++ tests/water05_test.rb
@@ -1,17 +1,20 @@
 #!/usr/bin/env ruby
 
-# gem install minitest (optional)
-# ruby tests/water01_test.rb
-
 require 'minitest/autorun'
-require_relative '../water01'
+require_relative '../new_code'
 
-class Water00Test < Minitest::Test
-  def test_combinations
-    expected = "00 01"
+class NewCodeTest < Minitest::Test
+  def test_manual_include
+    assert_equal true, manual_include("hello", "lo")
+    assert_equal false, manual_include("hello", "day")
+    assert_equal true, manual_include("rubyprogramming", "ruby")
+    assert_equal false, manual_include("rubyprogramming", "python")
+  end
 
-    result = generate_combinations.join(", ")
-    
-    assert_equal expected, result
+  def test_manual_include_with_empty_or_nil_inputs
+    assert_equal false, manual_include("", "lo")
+    assert_equal false, manual_include("hello", "")
+    assert_equal false, manual_include(nil, "lo")
+    assert_equal false, manual_include("hello", nil)
   end
 end
