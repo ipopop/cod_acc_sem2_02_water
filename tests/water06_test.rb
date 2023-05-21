662ab24bdabf6f4140d8271d5fe1041cfed5df56
ipopop <popop@ntymail.com>
2023-05-21 21:01:48 +0200

    update water06_test.rb

--- tests/water06_test.rb
+++ tests/water06_test.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/env ruby
 
-# gem install minitest (optional)
-# ruby tests/water01_test.rb
-
 require 'minitest/autorun'
-require_relative '../water01'
+require_relative '../new_code'
 
-class Water00Test < Minitest::Test
-  def test_combinations
-    expected = "00 01"
+class CapitalizeEveryOtherLetterTest < Minitest::Test
+  def test_capitalize_every_other_letter
+    assert_equal "HeLlO wOrLd!", capitalize_every_other_letter("Hello world!")
+    assert_equal "AbCdEfGhIj", capitalize_every_other_letter("abcdefghij")
+    assert_equal "123!@#AbC", capitalize_every_other_letter("123!@#abc")
+  end
 
-    result = generate_combinations.join(", ")
-    
-    assert_equal expected, result
+  def test_capitalize_every_other_letter_with_non_string_input
+    assert_equal '', capitalize_every_other_letter(42)
+    assert_equal '', capitalize_every_other_letter(nil)
   end
 end
